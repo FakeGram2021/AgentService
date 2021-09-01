fe296790a58328834db1444371a8a9db94946ee6
Nemanja Janković <nemanja997@gmail.com>
2021-09-01 19:09:35 +0200

    Agent service compose configuration

--- src/main/java/tim6/agentservice/adapter/kafka/configuration/KafkaConfiguration.java
+++ src/main/java/tim6/agentservice/adapter/kafka/configuration/KafkaConfiguration.java
@@ -4,6 +4,7 @@ import java.util.HashMap;
 import java.util.Map;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.annotation.EnableKafka;
@@ -17,10 +18,12 @@ import tim6.agentservice.adapter.kafka.models.KafkaMessage;
 @EnableKafka
 public class KafkaConfiguration {
 
+    @Value("${KAFKA_SERVER}") String kafkaBootstrapServer;
+
     @Bean
     public ConsumerFactory<String, KafkaMessage> kafkaMessageConsumer() {
         final Map<String, Object> configMap = new HashMap<>();
-        configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configMap.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaBootstrapServer);
         configMap.put(ConsumerConfig.GROUP_ID_CONFIG, "groupId");
         configMap.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configMap.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
